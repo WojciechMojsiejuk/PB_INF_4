fd7c343bc600612ba54a065eb5530f8f2b650c80
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2019-03-12 13:19:13 +0100

    Update 9.rb

--- RSWTA/PS02/9.rb
+++ RSWTA/PS02/9.rb
@@ -10,9 +10,38 @@ attr_accessor :liczbaowocow
     end
   def oneYearPasses
     @wiek=@wiek+1
-    @wzrost = @wzrost + Random.rand(11- @wzrost) 
+    if (@wzrost + Random.rand(1.0).round(1))<10
+      @wzrost = (@wzrost + Random.rand(1.0)).round(1)
+    else
+      @wzrost=10
+    end
+    if @wiek>5
+      @liczbaowocow=@liczbaowocow+@wiek-5
+    end 
   end
-
+  def to_s
+    if @wiek>50
+      "Drzewo pomaranczowe - drzewo jest martwe"
+    else
+      "Drzewo pomaranczowe - wiek: "+ @wiek.to_s + ", wysokosc: "+@wzrost.to_s+" m"
+    end
+  end
+  def countTheOranges
+    puts "Na drzewie wisi "+@liczbaowocow.to_s+" pomarancze" 
+  end
+  def pickAnOrange
+    if @liczbaowocow<=0
+      puts"Nie ma juz owocow do zerwania w tym roku"
+    else
+      puts "Pomarancza jest pyszna"
+      @liczbaowocow=@liczbaowocow-1
+    end
+  end
 end
 orange_tree = OrangeTree.new
+6.times {orange_tree.oneYearPasses}
 puts orange_tree
+puts orange_tree.countTheOranges
+2.times{orange_tree.pickAnOrange}
+puts orange_tree.liczbaowocow
+orange_tree.countTheOranges
