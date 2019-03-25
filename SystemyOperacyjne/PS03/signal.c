4ed7f441c61fc7c15a735f4e715a5b8ee60d8d47
Wojciech Mojsiejuk <wojciechmojsiejuk@macbook-pro-wojciech.home>
2019-03-25 14:55:33 +0100

    Syslog & signal handler

--- SystemyOperacyjne/PS03/signal.c
+++ SystemyOperacyjne/PS03/signal.c
@@ -1,20 +1,28 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 volatile sig_atomic_t keep_going = 1;
 
 void handler(int signum){
     printf("signal!\n");
-    keep_going = 0;
+    if (signum==SIGQUIT)
+    {
+        keep_going = 0;
+        syslog(LOG_INFO,"Handled signal, now closing program");
+    }
+    
     }
 
 int main(void){
     signal(SIGINT, handler);
+    syslog(LOG_INFO,"Working program");
     while(keep_going)
     {
         printf("working...\n");
         sleep(1);
     }
+    closelog();
     return 0;
 }
